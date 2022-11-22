ab58eb2f85bb7d33ab1f1bc6c8644e30f02669e6
Rafał Kowalczyk <raff1410X@gmail.com>
2022-11-22 16:39:52 +0100

    Update

--- components/translator.js
+++ components/translator.js
@@ -1,77 +1,89 @@
 // Dictionary objects
 const ameri = require('./american-only.js');
 let spells = require('./american-to-british-spelling.js');
-let titles = require("./american-to-british-titles.js")
-const briti = require('./british-only.js')
+let titles = require('./american-to-british-titles.js');
+const briti = require('./british-only.js');
 
 // Reverse object keys and values, return new object
-const reverseObj = (obj) => Object.fromEntries(Object.entries(obj).map(([key, value]) => [value, key]))
+const reverseObj = (obj) =>
+    Object.fromEntries(Object.entries(obj).map(([key, value]) => [value, key]));
 
 // Change object values - first letter to upper case, return new object
-const toUpper = (obj) => Object.fromEntries(Object.entries(obj).map(([key, value]) => 
-  [key, value.charAt(0).toUpperCase() + value.substring(1)]))
+const toUpper = (obj) =>
+    Object.fromEntries(
+        Object.entries(obj).map(([key, value]) => [
+            key,
+            value.charAt(0).toUpperCase() + value.substring(1),
+        ])
+    );
 
 // Change words in string to words from dictionary object, return string
 const rewriteFromObj = (text, translated, obj, highlight) => {
-  Object.keys(obj).forEach(el => {
-    const testEl =  new RegExp(`${el}[. ]`,'gi');
-    if (testEl.test(text)) {
-      const changeEl =  new RegExp(el,'gi');
-      if (highlight) {
-          translated = translated.replace(changeEl, highLight(obj[el]))
-          translated = translated.replaceAll('</span> <span class="highlight">', ' ')        
-      }
-      else translated = translated.replace(changeEl, obj[el])
-    }
-  })
-  return translated
-}
+    Object.keys(obj).forEach((el) => {
+        const testEl = new RegExp(`${el}[. ]`, 'gi');
+        if (testEl.test(text)) {
+            const changeEl = new RegExp(el, 'gi');
+            if (highlight) {
+                translated = translated.replace(changeEl, highLight(obj[el]));
+                translated = translated.replaceAll(
+                    '</span> <span class="highlight">',
+                    ' '
+                );
+            } else translated = translated.replace(changeEl, obj[el]);
+        }
+    });
+    return translated;
+};
 
 // Add <span class="highlight"> to string
-const highLight = (txt) => `<span class="highlight">${txt}</span>`
+const highLight = (txt) => `<span class="highlight">${txt}</span>`;
 
 // Change time format - "." and ":"
-const formatTime = (txt, arr, highlight) => txt.split(' ').map(el => {
-  if (/\d.\d{2}?/.test(el)) 
-    return (highlight) ? highLight(el.replace(arr[0], arr[1])) : el.replace(arr[0], arr[1])
-  return el
-}).join(' ')
+const formatTime = (txt, arr, highlight) => {
+    const regex = new RegExp(`([0-9]{1,2})([${arr[0]}])([0-9]{1,2})`, 'g');
+    if (highlight)
+        return txt.replace(
+            regex,
+            `<span class="highlight">$1${arr[1]}$3</span>`
+        );
+    return txt.replace(regex, `$1${arr[1]}$3`);
+};
 
 // Change string to lower case and remove spaces, return new string
-const flatString = (str) => str.toLowerCase().replace(/ /g,'');
+const flatString = (str) => str.toLowerCase().replace(/ /g, '');
 
 // Compare two strings, return true/false
-const checkStrings = (txt1, txt2) => (flatString(txt1) === flatString(txt2)) ? true : false
+const checkStrings = (txt1, txt2) =>
+    flatString(txt1) === flatString(txt2) ? true : false;
 
 // Main function, return translated string
-module.exports = function translate(text, locale, highlight) { 
-  let trans = ameri
-  let time = [':', '.']
-  let spell = spells
-  let title = titles
-  if (locale === 'british-to-american') {
-    trans = briti
-    time = ['.', ':']
-    spell = reverseObj(spells)
-    title = reverseObj(titles)
-  }
-  title = toUpper(title)
-  let translated = text
-  translated = rewriteFromObj(text, translated, spell, highlight)
-  translated = rewriteFromObj(text, translated, trans, highlight)
-  translated = rewriteFromObj(text, translated, title, highlight)
-  translated = formatTime(translated, time, highlight)
-  if (checkStrings(translated, text)) 
-    return 'Everything looks good to me!' 
-  return translated
-}
+module.exports = function translate(text, locale, highlight) {
+    let trans = ameri;
+    let time = [':', '.'];
+    let spell = spells;
+    let title = titles;
+    if (locale === 'british-to-american') {
+        trans = briti;
+        time = ['.', ':'];
+        spell = reverseObj(spells);
+        title = reverseObj(titles);
+    }
+    title = toUpper(title);
+    let translated = text;
+    translated = rewriteFromObj(text, translated, spell, highlight);
+    translated = rewriteFromObj(text, translated, trans, highlight);
+    translated = rewriteFromObj(text, translated, title, highlight);
+    translated = formatTime(translated, time, highlight);
+    if (checkStrings(translated, text)) return 'Everything looks good to me!';
+    return translated;
+};
 
 // Alternative solution
 
 // const reverseObj = (obj) => {
 //   return Object.fromEntries(Object.entries(obj).map(([key, value]) => [value, key]))
 // }
-  
+
 // const translateArr = (arr, obj) => {
 //   return arr.map(el => {
 //       if (Object.keys(obj).includes(el) && !Object.keys(obj).includes(' '))
@@ -129,7 +141,7 @@ module.exports = function translate(text, locale, highlight) {
 //   return false
 // }
 
-// module.exports = function translate(text, locale, highlight) { 
+// module.exports = function translate(text, locale, highlight) {
 //   let txt = text
 //   let copy = text.split(' ')
 //   let last = ''
@@ -138,7 +150,7 @@ module.exports = function translate(text, locale, highlight) {
 //     copy = text.slice(0, -1).split(' ')
 //     last = text.slice(-1)
 //   }
-  
+
 //   let trans = ameri
 //   let time = [':', '.']
 //   let spell = spells
@@ -149,7 +161,7 @@ module.exports = function translate(text, locale, highlight) {
 //     spell = reverseObj(spells)
 //     title = reverseObj(titles)
 //   }
-  
+
 //   let ret = txt.toLowerCase().split(' ');
 //   ret = translateTwoArr(ret, trans)
 //   ret = translateArr(ret, trans)
@@ -157,10 +169,10 @@ module.exports = function translate(text, locale, highlight) {
 //   ret = translateArr(ret, title)
 //   ret = copyLetter(ret, copy)
 //   ret = formatTime(ret, time)
-  
+
 //   if (checkStrings(ret.join(' '), txt))
-//     return 'Everything looks good to me!' 
-//   if (highlight) 
+//     return 'Everything looks good to me!'
+//   if (highlight)
 //     return highLight(ret, copy).join(' ') + last
 //   return ret.join(' ') + last
 // }
